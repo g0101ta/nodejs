763524380e946a39976cbf75e25bb40d25ecaadc
Akihiro Takagi <g0101ta@gmail.com>
2025-04-29 09:55:05 +0900

    app.jsにWebSocketストリームの接続を追加し、レスポンスメッセージを修正。package.jsonとbun.lockにnodemonを追加。

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ const app = express();
 const PORT = 3000;
 
 app.get('/api', (req, res) => {
+  const wssUrl = `ws://${req.headers.host}/media-stream`;
+  console.log(wssUrl);
   const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
                           <Response>
                               <Say>Please wait while we connect your call to the A. I. voice assistant, powered by Twilio and the Open-A.I. Realtime API</Say>
                               <Pause length="1"/>
-                              <Say>O.K. you can start talking!</Say>
+                              <Say>you can start talking!</Say>
+                              <Connect>
+                                  <Stream url="${wssUrl}" />
+                              </Connect>
                           </Response>`;
-  res.send(twimlResponse);
+  res.status(200).type('text/xml').send(twimlResponse);
 });
 
 app.listen(PORT, () => {
